b222c4ee63e8f7e01b518b4df8ffa965b0f6af26
matlencar <malencar292@gmail.com>
2022-02-07 20:51:52 -0300

    Continuação exercicio de estruturas

---
+++ estruturas-repetitivas/src/estruturas/exemplo/ExercicioFor6.java
@@ -0,0 +1,21 @@
+package estruturas.exemplo;
+
+import java.util.Scanner;
+
+public class ExercicioFor6 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			
+			if( n % i == 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
